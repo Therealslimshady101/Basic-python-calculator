d9e6f00707a0475033a0603f4c3da2d263a255d6
Therealslimshady101 <franceh405@gmail.com>
2025-03-20 19:24:06 +0000

    Add files via upload

---
+++ Basic calculator.py
@@ -0,0 +1,16 @@
+print ("Please use either add, subtract, divide ir multiply")
+answer1 = input ("Enter your first number")
+answer2 = input ("Enter your second number")
+answer3 = input ("Enter your desired function")
+if answer3 == ("add"):
+    sum = int(answer1) + int (answer2)
+    print (sum)
+if answer3 == ("subtract"):
+    sum = int (answer1) - int (answer2)
+    print (sum)
+if answer3 == ("divide"):
+    sum = int (answer1) / int (answer2)
+    print (sum)
+if answer3 == ("multiply"):
+    sum = int (answer1) * int (answer2)
+    print (sum)
